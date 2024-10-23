b926ef76c427af3564a479d31186cb1cf61d30a0
Caio Assis <caio.ab.assis@gmail.com>
2024-10-23 16:13:37 -0300

    cria CRUD user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express'
 import equipmentRoutes from './equipments-routes'
 import maintenanceRoutes from './maintenance-routes'
 import usageRoutes from './usage-routes'
+import userRoutes from './user-routes'
 
 const routes = Router()
 
 routes.use('/equipment', equipmentRoutes)
 routes.use('/maintenance', maintenanceRoutes)
 routes.use('/usage', usageRoutes)
+routes.use('/user', userRoutes)
 
 export default routes
 
