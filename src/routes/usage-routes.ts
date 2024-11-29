35af993d78c20e2c7ec57a944830c752ccf2debc
Caio Assis <caio.ab.assis@gmail.com>
2024-11-28 22:04:47 -0300

    adiciona getListById

--- src/routes/usage-routes.ts
+++ src/routes/usage-routes.ts
@@ -5,6 +5,7 @@ const usageRoutes = Router()
 
 usageRoutes.get('/', UsageController.getUsageRecords)
 usageRoutes.get('/:id', UsageController.getUsageRecordById)
+usageRoutes.get('/all/:id', UsageController.getUsageRecordListById)
 usageRoutes.post('/', UsageController.createUsageRecord)
 usageRoutes.put('/:id', UsageController.updateUsageRecord)
 usageRoutes.delete('/:id', UsageController.removeUsageRecord)
