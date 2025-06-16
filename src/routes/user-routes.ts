0cc1eb9bcb66e2b79b653acfdc50748d5549ad15
Michael Douglas <michaeldouglasramos2010@gmail.com>
2025-06-15 22:08:30 -0300

    adjust sequence of route of user

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -3,9 +3,9 @@ import UserController from '../controllers/user-controller'
 
 const userRoutes = Router()
 
+userRoutes.get('/login/:em', UserController.getUserByEmail)
 userRoutes.get('/', UserController.getUser)
 userRoutes.get('/:id', UserController.getUserById)
-userRoutes.get('/login/:em', UserController.getUserByEmail)
 userRoutes.post('/', UserController.createUser)
 userRoutes.put('/:id', UserController.updateUser)
 userRoutes.delete('/:id', UserController.removeUser)
